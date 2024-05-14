ffce3c92d40fed46e9e7ee93968b64e24167d542
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-14 16:09:17 +0200

    Calc MilliSeconds

--- src/main/java/de/rayzs/pat/utils/TimeConverter.java
+++ src/main/java/de/rayzs/pat/utils/TimeConverter.java
@@ -2,25 +2,29 @@ package de.rayzs.pat.utils;
 
 public class TimeConverter {
 
-    private static final int MINUTES = 60, HOURS = MINUTES * 60, DAYS = HOURS * 24, WEEKS = DAYS * 7;
+    private static final int SECONDS = 1000, MINUTES = SECONDS * 60, HOURS = MINUTES * 60, DAYS = HOURS * 24, WEEKS = DAYS * 7;
 
     public static String calcAndGetTime(long time) {
-        long upTime = (System.currentTimeMillis() - time) / 1000;
-        int weeks = 0, days = 0, hours = 0, minutes = 0, seconds = (int) upTime;
+        long upTime = (System.currentTimeMillis() - time);
+        int weeks = 0, days = 0, hours = 0, minutes = 0, seconds = 0, ms = (int) upTime;
 
-        while (seconds >= 60) {
-            if (seconds >= WEEKS) {
+        while (ms >= SECONDS) {
+            if (ms >= WEEKS) {
                 weeks++;
-                seconds = seconds - WEEKS;
-            } else if (seconds >= DAYS) {
+                ms = ms - WEEKS;
+            } else if (ms >= DAYS) {
                 days++;
-                seconds = seconds - DAYS;
-            } else if (seconds >= HOURS) {
+                ms = ms - DAYS;
+            } else if (ms >= HOURS) {
                 hours++;
-                seconds = seconds - HOURS;
+                ms = ms - HOURS;
+            } else if (ms >= MINUTES) {
+                minutes++;
+                ms = ms - MINUTES;
+            } else {
+                seconds++;
+                ms = ms - SECONDS;
             }
-            minutes++;
-            seconds = seconds - MINUTES;
         }
 
         StringBuilder uptimeText = new StringBuilder();
@@ -28,7 +32,8 @@ public class TimeConverter {
         else if(days > 0) uptimeText.append(days + "d, ");
         else if(hours > 0) uptimeText.append(hours + "h, ");
         else if(minutes > 0) uptimeText.append(minutes + "m, ");
-        uptimeText.append(seconds + "s");
+        else if(seconds > 0) uptimeText.append(seconds + "s");
+        else uptimeText.append(ms + "ms");
         return uptimeText.toString();
     }
 }
