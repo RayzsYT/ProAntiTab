09fb21471a9bf39019d6515ac5373d10da9f0ac8
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-16 10:48:28 +0200

    Fix 1.20.6 Support

--- src/main/java/de/rayzs/pat/plugin/netty/handlers/ModernPacketHandler.java
+++ src/main/java/de/rayzs/pat/plugin/netty/handlers/ModernPacketHandler.java
@@ -1,5 +1,6 @@
 package de.rayzs.pat.plugin.netty.handlers;
 
+import com.mojang.brigadier.suggestion.Suggestion;
 import com.mojang.brigadier.suggestion.Suggestions;
 import de.rayzs.pat.plugin.logger.Logger;
 import de.rayzs.pat.plugin.netty.PacketAnalyzer;
@@ -7,6 +8,9 @@ import de.rayzs.pat.plugin.netty.PacketHandler;
 import org.bukkit.entity.Player;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import de.rayzs.pat.utils.*;
 
@@ -41,26 +45,36 @@ public class ModernPacketHandler implements PacketHandler {
             return false;
         }
 
-        boolean cancelsBeforeHand = false;
+        boolean cancelsBeforeHand = false,
+                // 1.20.6 support coming soon bcs it's to annoying to work with on packet-base rn
+                weird = Reflection.getMinor() == 20 && Reflection.getRelease() >= 6 || Reflection.getMinor() >= 20;
+        int spaces = 0;
 
         if(input.startsWith("/")) {
             input = input.replace("/", "");
-            if(input.contains(" ")) input = input.split(" ")[0];
+            String[] split = input.split(" ");
+            if(input.contains(" ")) input = split[0];
 
-            cancelsBeforeHand = Storage.isCommandBlockedPrecise(input) && !PermissionUtil.hasBypassPermission(player, input)
-                    || Storage.isCommandBlocked(input) && !PermissionUtil.hasBypassPermission(player, input);
+            cancelsBeforeHand = !Storage.TURN_BLACKLIST_TO_WHITELIST && Storage.isCommandBlockedPrecise(input) && !PermissionUtil.hasBypassPermission(player, input)
+                    || Storage.TURN_BLACKLIST_TO_WHITELIST && !Storage.isCommandBlocked(input) && !PermissionUtil.hasBypassPermission(player, input);
         }
 
         Suggestions suggestions = (Suggestions) suggestionsMethod.invoke(packetObj);
+        if(input.length() < 1 && weird || cancelsBeforeHand && weird)
+            return false;
+        else if(cancelsBeforeHand)
+            suggestions.getList().clear();
+        else {
+            if(weird) return true;
 
-        if(!cancelsBeforeHand)
             suggestions.getList().removeIf(suggestion -> {
                 String command = suggestion.getText();
-                if(Storage.TURN_BLACKLIST_TO_WHITELIST)
+                if (Storage.TURN_BLACKLIST_TO_WHITELIST)
                     return !Storage.isCommandBlockedPrecise(command) && !PermissionUtil.hasBypassPermission(player, command);
                 else return Storage.isCommandBlocked(command) && !PermissionUtil.hasBypassPermission(player, command);
             });
-        else suggestions.getList().clear();
+        }
+
 
         return true;
     }
