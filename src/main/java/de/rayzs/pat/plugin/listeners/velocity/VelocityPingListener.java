ee7aafe3d3ae014f3e792923d8e7310f5112fbd4
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-17 12:16:15 +0200

    Initialize Server Variable

--- src/main/java/de/rayzs/pat/plugin/listeners/velocity/VelocityPingListener.java
+++ src/main/java/de/rayzs/pat/plugin/listeners/velocity/VelocityPingListener.java
@@ -9,12 +9,17 @@ import de.rayzs.pat.utils.Storage;
 
 public class VelocityPingListener {
 
+    private static ProxyServer server;
+
+    public VelocityPingListener(ProxyServer server) {
+        VelocityPingListener.server = server;
+    }
+
     @Subscribe
     public void onProxyPing(ProxyPingEvent event) {
         if(!Storage.USE_CUSTOM_PROTOCOL_PING) return;
 
-        ProxyServer proxyServer = VelocityLoader.getServer();
-        int online = proxyServer.getPlayerCount(), max = proxyServer.getConfiguration().getShowMaxPlayers();
+        int online = server.getPlayerCount(), max = server.getConfiguration().getShowMaxPlayers();
         ServerPing oldServerPing = event.getPing(), newServerPing;
         ServerPing.Version newVersion = new ServerPing.Version(!Storage.USE_ALWAYS_SHOW_CUSTOM_PROTOCOL_PING ? oldServerPing.getVersion().getProtocol() : -1, Storage.CUSTOM_PROTOCOL_PING.replace("%online%", String.valueOf(online)).replace("%max%", String.valueOf(max)));
         newServerPing = oldServerPing.asBuilder().version(newVersion).build();
