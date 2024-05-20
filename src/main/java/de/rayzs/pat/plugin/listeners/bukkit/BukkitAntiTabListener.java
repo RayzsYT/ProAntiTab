4519de59427a50ead281da6abe1a5f0753308541
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-20 22:44:36 +0200

    Fix : Tab

--- src/main/java/de/rayzs/pat/plugin/listeners/bukkit/BukkitAntiTabListener.java
+++ src/main/java/de/rayzs/pat/plugin/listeners/bukkit/BukkitAntiTabListener.java
@@ -17,7 +17,7 @@ public class BukkitAntiTabListener implements Listener {
         if (PermissionUtil.hasBypassPermission(player)) return;
 
         if(!BukkitLoader.isLoaded()) event.getCommands().clear();
-        else event.getCommands().removeIf(command -> Storage.BLACKLIST.isBlocked(player, command));
+        else event.getCommands().removeIf(command -> Storage.BLACKLIST.isBlocked(player, command, true));
     }
 
     public static void updateCommands() {
