c02be74b432cd0d70f2c587ba75af52b3c6556d5
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-28 23:31:59 +0200

    Add Check

--- src/main/java/de/rayzs/pat/plugin/listeners/velocity/VelocityAntiTabListener.java
+++ src/main/java/de/rayzs/pat/plugin/listeners/velocity/VelocityAntiTabListener.java
@@ -20,8 +20,7 @@ public class VelocityAntiTabListener {
     @Subscribe (order = PostOrder.LAST)
     public void onTabComplete(TabCompleteEvent event) {
         Player player = event.getPlayer();
-
-        if(PermissionUtil.hasBypassPermission(player)) return;
+        if(PermissionUtil.hasBypassPermission(player) || event.getSuggestions().isEmpty()) return;
 
         event.getSuggestions().removeIf(command -> Storage.Blacklist.isBlocked(player, command, !Storage.ConfigSections.Settings.TURN_BLACKLIST_TO_WHITELIST.ENABLED, player.getCurrentServer().get().getServerInfo().getName()));
     }
@@ -29,8 +28,7 @@ public class VelocityAntiTabListener {
     @Subscribe (order = PostOrder.LAST)
     public void onPlayerAvailableCommands(PlayerAvailableCommandsEvent event) {
         Player player = event.getPlayer();
-
-        if(PermissionUtil.hasBypassPermission(player)) return;
+        if(PermissionUtil.hasBypassPermission(player) || event.getRootNode().getChildren().isEmpty()) return;
 
         event.getRootNode().getChildren().removeIf(command -> Storage.Blacklist.isBlocked(player, command.getName(), !Storage.ConfigSections.Settings.TURN_BLACKLIST_TO_WHITELIST.ENABLED, player.getCurrentServer().get().getServerInfo().getName()));
     }
