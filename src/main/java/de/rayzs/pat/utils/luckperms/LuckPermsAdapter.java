41ce4b1377e111ace13938fdda93ae8fbfddc7dd
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-29 16:31:52 +0200

    Create

---
+++ src/main/java/de/rayzs/pat/utils/luckperms/LuckPermsAdapter.java
@@ -0,0 +1,45 @@
+package de.rayzs.pat.utils.luckperms;
+
+import de.rayzs.pat.utils.permission.PermissionUtil;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.event.EventBus;
+import net.luckperms.api.event.node.*;
+import net.luckperms.api.node.*;
+import net.luckperms.api.*;
+
+public class LuckPermsAdapter {
+
+    public static void initialize() {
+        LuckPerms provider = LuckPermsProvider.get();
+        EventBus eventBus = provider.getEventBus();
+
+        eventBus.subscribe(provider, NodeAddEvent.class, LuckPermsAdapter::onNodeAdd);
+        eventBus.subscribe(provider, NodeRemoveEvent.class, LuckPermsAdapter::onNodeRemove);
+    }
+
+    private static void onNodeAdd(NodeAddEvent event) {
+        if (!event.isUser()) return;
+        Node node = event.getNode();
+
+        if (node.getType() != NodeType.INHERITANCE
+                || !(event.getTarget() instanceof User)
+                || !node.getKey().startsWith("proantitab."))
+            return;
+
+        User user = (User) event.getTarget();
+        PermissionUtil.resetPermissions(user.getUniqueId());
+    }
+
+    private static void onNodeRemove(NodeRemoveEvent event) {
+        if (!event.isUser()) return;
+        Node node = event.getNode();
+
+        if (node.getType() != NodeType.INHERITANCE
+                || !(event.getTarget() instanceof User)
+                || !node.getKey().startsWith("proantitab."))
+            return;
+
+        User user = (User) event.getTarget();
+        PermissionUtil.resetPermissions(user.getUniqueId());
+    }
+}
