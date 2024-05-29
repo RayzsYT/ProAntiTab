775a841d5621ec1ec4fa06ff6d97cdabe66a9722
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-05-29 10:19:05 +0200

    Add

---
+++ src/main/java/de/rayzs/pat/utils/PermissionStorage.java
@@ -0,0 +1,24 @@
+package de.rayzs.pat.utils;
+
+import java.util.*;
+
+public class PermissionStorage {
+
+    private final UUID uuid;
+    private final HashMap<String, Boolean> permissionMap = new HashMap<>();
+
+    public PermissionStorage(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public PermissionState getPermissionState(String permission) {
+        if(!hasPermissionState(permission)) return PermissionState.EMPTY;
+        return permissionMap.get(permission) ? PermissionState.PERMITTED : PermissionState.DENIED;
+    }
+
+    public boolean hasPermissionState(String permission) {
+        return permissionMap.containsKey(permission);
+    }
+
+    enum PermissionState { PERMITTED, DENIED, EMPTY }
+}
