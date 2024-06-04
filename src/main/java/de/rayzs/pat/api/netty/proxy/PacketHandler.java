0f5d9d2fab8e912c800ab8573ddd3a98f43cc3ac
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-06-04 21:33:52 +0200

    Remove

--- src/main/java/de/rayzs/pat/api/netty/proxy/PacketHandler.java
+++
@@ -1,8 +0,0 @@
-package de.rayzs.pat.api.netty.proxy;
-
-import java.util.UUID;
-
-public interface PacketHandler {
-    boolean handleIncomingPacket(UUID uuid, Object packetObj) throws Exception;
-    boolean handleOutgoingPacket(UUID uuid, Object packetObj) throws Exception;
-}
