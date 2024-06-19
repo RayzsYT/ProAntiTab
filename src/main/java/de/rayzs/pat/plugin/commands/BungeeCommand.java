95b4c860301633f4371eac69b4bb0dc808c50667
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-06-19 17:12:47 +0200

    Remove Player

--- src/main/java/de/rayzs/pat/plugin/commands/BungeeCommand.java
+++ src/main/java/de/rayzs/pat/plugin/commands/BungeeCommand.java
@@ -2,7 +2,6 @@ package de.rayzs.pat.plugin.commands;
 
 import de.rayzs.pat.plugin.process.CommandProcess;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.*;
 
 public class BungeeCommand extends Command implements TabExecutor {
@@ -13,12 +12,6 @@ public class BungeeCommand extends Command implements TabExecutor {
 
     @Override
     public void execute(CommandSender commandSender, String[] strings) {
-        if(commandSender instanceof ProxiedPlayer) {
-            ProxiedPlayer proxiedPlayer = (ProxiedPlayer) commandSender;
-            CommandProcess.handleCommand(proxiedPlayer, strings, "bpat");
-            return;
-        }
-
         CommandProcess.handleCommand(commandSender, strings, "bpat");
     }
 
--- src/main/java/de/rayzs/pat/plugin/commands/VelocityCommand.java
+++ src/main/java/de/rayzs/pat/plugin/commands/VelocityCommand.java
@@ -1,7 +1,6 @@
 package de.rayzs.pat.plugin.commands;
 
 import com.velocitypowered.api.command.SimpleCommand;
-import com.velocitypowered.api.proxy.Player;
 import de.rayzs.pat.plugin.process.CommandProcess;
 import java.util.List;
 
@@ -9,12 +8,6 @@ public class VelocityCommand implements SimpleCommand {
 
     @Override
     public void execute(Invocation invocation) {
-        if(invocation.source() instanceof Player) {
-            Player player = (Player) invocation.source();
-            CommandProcess.handleCommand(player, invocation.arguments(), "bpat");
-            return;
-        }
-
         CommandProcess.handleCommand(invocation.source(), invocation.arguments(), "bpat");
     }
 
