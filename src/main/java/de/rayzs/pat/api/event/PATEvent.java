6f7729a83a850c5c72d597f061b44b68062cc739
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-08 04:09:30 +0200

    Add EventManager

---
+++ src/main/java/de/rayzs/pat/api/event/PATEvent.java
@@ -0,0 +1,9 @@
+package de.rayzs.pat.api.event;
+
+public abstract class PATEvent {
+
+    public Situation situation;
+    public abstract boolean shouldHandleByDefault(Object executor, String command);
+
+    public static enum Situation { EXECUTED_PLUGINS_COMMAND, EXECUTED_VERSION_COMMAND, EXECUTED_BLOCKED_COMMAND }
+}
---
+++ src/main/java/de/rayzs/pat/api/event/PATEventManager.java
@@ -0,0 +1,30 @@
+package de.rayzs.pat.api.event;
+
+import java.util.*;
+
+public class PATEventManager {
+
+    private static final List<PATEvent> EVENTS = new LinkedList<>();
+
+    public static void register(PATEvent event) {
+        if(!EVENTS.contains(event))
+            EVENTS.add(event);
+    }
+
+    public static void unregister(PATEvent event) {
+        EVENTS.remove(event);
+    }
+
+    public static void unregisterAll(PATEvent event) {
+        EVENTS.clear();
+    }
+
+    public static boolean useDefaultActions(Object executor, String command, PATEvent.Situation situation) {
+        for (PATEvent event : EVENTS) {
+            if(event.situation != situation) continue;
+            if(event.shouldHandleByDefault(executor, command)) return true;
+        }
+
+        return false;
+    }
+}
