0f4ede405bcdedb460e354982dc29a06e8d610bc
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-11 02:18:08 +0200

    Change Update-Message

--- src/main/java/de/rayzs/pat/api/storage/config/messages/UpdatePermissionsSection.java
+++ src/main/java/de/rayzs/pat/api/storage/config/messages/UpdatePermissionsSection.java
@@ -5,7 +5,7 @@ import de.rayzs.pat.api.storage.storages.ConfigStorage;
 
 public class UpdatePermissionsSection extends ConfigStorage {
 
-    public String UPDATED;
+    public String UPDATE_ALL, UPDATE_SPECIFIC;
 
     public UpdatePermissionsSection() {
         super("update-permissions");
@@ -14,6 +14,7 @@ public class UpdatePermissionsSection extends ConfigStorage {
     @Override
     public void load() {
         super.load();
-        UPDATED = new ConfigSectionHelper<String>(this, null, "&aUpdated permissions!").getOrSet();
+        UPDATE_ALL = new ConfigSectionHelper<String>(this, "all-players", "&aUpdated permissions!").getOrSet();
+        UPDATE_SPECIFIC = new ConfigSectionHelper<String>(this, "specific-player", "&aUpdated %target%'s permissions!").getOrSet();
     }
 }
