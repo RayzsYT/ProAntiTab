e601d0831e324099885ee9040e06d841e0268eb2
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-15 17:11:38 +0200

    Add NameSpace Feature

---
+++ src/main/java/de/rayzs/pat/api/storage/config/settings/NamespaceSection.java
@@ -0,0 +1,25 @@
+package de.rayzs.pat.api.storage.config.settings;
+
+import de.rayzs.pat.utils.configuration.helper.ConfigSectionHelper;
+import de.rayzs.pat.api.storage.storages.ConfigStorage;
+import de.rayzs.pat.utils.StringUtils;
+
+public class NamespaceSection extends ConfigStorage {
+
+    public boolean ENABLED;
+
+    public NamespaceSection() {
+        super("block-namespace-commands");
+    }
+
+    @Override
+    public void load() {
+        super.load();
+        ENABLED = new ConfigSectionHelper<Boolean>(this, "enabled", true).getOrSet();
+    }
+
+    public boolean isCommand(String command) {
+        if(!ENABLED) return false;
+        return StringUtils.getFirstArg(command).contains(":");
+    }
+}
