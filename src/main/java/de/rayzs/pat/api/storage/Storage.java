686358e33a71d22e22adb8facf81865a3b6914f1
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-15 17:17:20 +0200

    Add Namespace Section

--- src/main/java/de/rayzs/pat/api/storage/Storage.java
+++ src/main/java/de/rayzs/pat/api/storage/Storage.java
@@ -103,6 +103,7 @@ public class Storage {
 
         public static class Settings {
 
+            public static BlockNamespaceCommandsSection BLOCK_NAMESPACE_COMMANDS = new BlockNamespaceCommandsSection();
             public static HandleThroughProxySection HANDLE_THROUGH_PROXY = new HandleThroughProxySection();
             public static CustomBrandSection CUSTOM_BRAND = new CustomBrandSection();
             public static CancelCommandSection CANCEL_COMMAND = new CancelCommandSection();
