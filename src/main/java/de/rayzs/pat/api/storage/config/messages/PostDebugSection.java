37a78449a7ec6b4f08e5dd7b9335f4dbd38c1c3a
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-17 04:14:27 +0200

    Change Default PostDebug Message

--- src/main/java/de/rayzs/pat/api/storage/config/messages/PostDebugSection.java
+++ src/main/java/de/rayzs/pat/api/storage/config/messages/PostDebugSection.java
@@ -18,7 +18,7 @@ public class PostDebugSection extends ConfigStorage {
     @Override
     public void load() {
         super.load();
-        SUCCESS = new ConfigSectionHelper<String>(this, "success", "&aSuccessfully uploaded debug logs: &e%link%").getOrSet();
+        SUCCESS = new ConfigSectionHelper<String>(this, "success", "&aSuccessfully uploaded debug logs: &e%link% &8(<click:open_url:%link%><gray>CLICK</click>&8)").getOrSet();
         FAILED = new ConfigSectionHelper<String>(this, "failed", "&cFailed to upload debug logs!").getOrSet();
     }
 }
