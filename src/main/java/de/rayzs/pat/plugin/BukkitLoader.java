969ac6632e6ac96511f116a414ac8cb441d99643
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-17 04:25:41 +0200

    Move NameSpace Packet

--- src/main/java/de/rayzs/pat/plugin/BukkitLoader.java
+++ src/main/java/de/rayzs/pat/plugin/BukkitLoader.java
@@ -165,11 +165,14 @@ public class BukkitLoader extends JavaPlugin {
         Storage.LAST_SYNC = System.currentTimeMillis();
         Communicator.sendFeedback();
         CommunicationPackets.UnknownCommandPacket unknownCommandPacket = packetBundle.getUnknownCommandPacket();
-        CommunicationPackets.NamespaceCommandsPacket namespaceCommandsPacket = packetBundle.getNamespaceCommandsPacket();
 
         if(!Storage.USE_VELOCITY) {
             CommunicationPackets.CommandsPacket commandsPacket = packetBundle.getCommandsPacket();
             CommunicationPackets.GroupsPacket groupsPacket = packetBundle.getGroupsPacket();
+            CommunicationPackets.NamespaceCommandsPacket namespaceCommandsPacket = packetBundle.getNamespaceCommandsPacket();
+
+            if(Storage.ConfigSections.Settings.BLOCK_NAMESPACE_COMMANDS.ENABLED != namespaceCommandsPacket.isEnabled())
+                Storage.ConfigSections.Settings.BLOCK_NAMESPACE_COMMANDS.ENABLED = namespaceCommandsPacket.isEnabled();
 
             if (commandsPacket.getCommands() == null || commandsPacket.getCommands().isEmpty())
                 Storage.Blacklist.getBlacklist().setList(new ArrayList<>());
@@ -189,9 +192,6 @@ public class BukkitLoader extends JavaPlugin {
         Storage.ConfigSections.Settings.CUSTOM_UNKNOWN_COMMAND.MESSAGE = unknownCommandPacket.getMessage();
         if(Storage.ConfigSections.Settings.CUSTOM_UNKNOWN_COMMAND.ENABLED != unknownCommandPacket.isEnabled())
             Storage.ConfigSections.Settings.CUSTOM_UNKNOWN_COMMAND.ENABLED = unknownCommandPacket.isEnabled();
-
-        if(Storage.ConfigSections.Settings.BLOCK_NAMESPACE_COMMANDS.ENABLED != namespaceCommandsPacket.isEnabled())
-            Storage.ConfigSections.Settings.BLOCK_NAMESPACE_COMMANDS.ENABLED = namespaceCommandsPacket.isEnabled();
 
         if(!loaded) loaded = true;
 
