9a2f007399af387c6902cb5864b928463bbba05b
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-07-28 21:31:09 +0200

    Small Bug Fix

--- src/main/java/de/rayzs/pat/plugin/process/CommandProcess.java
+++ src/main/java/de/rayzs/pat/plugin/process/CommandProcess.java
@@ -24,7 +24,7 @@ public class CommandProcess {
     private static final ExpireCache<UUID, String> CONFIRMATION = new ExpireCache<>(4, TimeUnit.SECONDS);
 
     public static void handleCommand(Object senderObj, String[] args, String label) {
-        Logger.debug("Execute PAT command: /" + label + " " + StringUtils.buildStringListWithoutColors(Arrays.asList(args), " ", true));
+        Logger.debug("Execute PAT command: /" + label + " " + StringUtils.buildStringListWithoutColors(Arrays.asList(args.clone()), " ", true));
 
         CommandSender sender = new CommandSender(senderObj);
 
