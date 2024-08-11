e66cbf0da13120ae5264131b9ec75774ca924019
Alihan <51991564+RayzsYT@users.noreply.github.com>
2024-08-11 12:55:49 +0200

    Update PacketUtils.java

--- src/main/java/de/rayzs/pat/utils/PacketUtils.java
+++ src/main/java/de/rayzs/pat/utils/PacketUtils.java
@@ -53,6 +53,7 @@ public class PacketUtils {
     }
 
    /*
+        Copyright (c) 2012, md_5. All rights reserved.
         Original code from Bungeecord source:
         https://github.com/SpigotMC/BungeeCord/blob/master/protocol/src/main/java/net/md_5/bungee/protocol/DefinedPacket.java#L263C1-L283C6
      */
@@ -71,6 +72,7 @@ public class PacketUtils {
     }
 
     /*
+        Copyright (c) 2012, md_5. All rights reserved.
         Original code from Bungeecord source:
         https://github.com/SpigotMC/BungeeCord/blob/master/protocol/src/main/java/net/md_5/bungee/protocol/DefinedPacket.java#L50C4-L71C1
      */
