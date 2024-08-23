516003398decd884e9fb36acee33d5cf243df87e
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-08-23 03:10:27 +0200

    Simplified Instance-Creation

--- src/main/java/de/rayzs/pat/api/event/events/ReceiveSyncEvent.java
+++ src/main/java/de/rayzs/pat/api/event/events/ReceiveSyncEvent.java
@@ -1,13 +1,17 @@
 package de.rayzs.pat.api.event.events;
 
-import de.rayzs.pat.api.event.PATEvent;
 import de.rayzs.pat.utils.CommunicationPackets;
-
+import de.rayzs.pat.api.event.PATEvent;
 import java.util.UUID;
 
 public abstract class ReceiveSyncEvent extends PATEvent<ReceiveSyncEvent> {
 
     private final CommunicationPackets.PacketBundle packetBundle;
+
+    public ReceiveSyncEvent() {
+        super(null);
+        this.packetBundle = null;
+    }
 
     public ReceiveSyncEvent(UUID senderUniqueId, CommunicationPackets.PacketBundle packetBundle) {
         super(senderUniqueId);
--- src/main/java/de/rayzs/pat/api/event/events/SentSyncEvent.java
+++ src/main/java/de/rayzs/pat/api/event/events/SentSyncEvent.java
@@ -1,14 +1,19 @@
 package de.rayzs.pat.api.event.events;
 
-import de.rayzs.pat.api.event.PATEvent;
 import de.rayzs.pat.utils.CommunicationPackets;
-
+import de.rayzs.pat.api.event.PATEvent;
 import java.util.UUID;
 
 public abstract class SentSyncEvent extends PATEvent<SentSyncEvent> {
 
     private final CommunicationPackets.PacketBundle packetBundle;
     private final String serverName;
+
+    public SentSyncEvent() {
+        super(null);
+        this.packetBundle = null;
+        this.serverName = null;
+    }
 
     public SentSyncEvent(UUID senderUniqueId, CommunicationPackets.PacketBundle packetBundle, String serverName) {
         super(senderUniqueId);
