e546dad6f3bebe1ccb2215dfaf6843f8e9c35453
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-09-18 03:06:47 +0200

    Change %target% -> %player%

--- src/main/java/de/rayzs/pat/api/storage/config/messages/PermsCheckSection.java
+++ src/main/java/de/rayzs/pat/api/storage/config/messages/PermsCheckSection.java
@@ -16,6 +16,6 @@ public class PermsCheckSection extends ConfigStorage {
         super.load();
         MESSAGE = new ConfigSectionHelper<String>(this, "message", "&7All of &e%player%'s &7PAT-related permission: &e%permissions%").getOrSet();
         PLAYER_MISSING = new ConfigSectionHelper<String>(this, "player-is-missing", "&cPlease specify the player whose permission you want to check.").getOrSet();
-        PLAYER_NOT_ONLINE = new ConfigSectionHelper<String>(this, "player-not-online", "&c%target% is not online!").getOrSet();
+        PLAYER_NOT_ONLINE = new ConfigSectionHelper<String>(this, "player-not-online", "&c%player% is not online!").getOrSet();
     }
 }
