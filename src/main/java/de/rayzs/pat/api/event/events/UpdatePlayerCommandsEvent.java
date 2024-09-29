c70aeefc447cbd663793423e5a075a7dd6c7ac94
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-09-29 15:40:02 +0200

    Modify

--- src/main/java/de/rayzs/pat/api/event/events/UpdatePlayerCommandsEvent.java
+++ src/main/java/de/rayzs/pat/api/event/events/UpdatePlayerCommandsEvent.java
@@ -5,11 +5,24 @@ import java.util.List;
 
 public abstract class UpdatePlayerCommandsEvent extends PATEvent<UpdatePlayerCommandsEvent> {
 
+    private List<String> commands;
+    private boolean serverBased;
+
     public UpdatePlayerCommandsEvent() {
         super(null);;
     }
 
     public UpdatePlayerCommandsEvent(Object senderObj, List<String> commands, boolean serverBased) {
         super(senderObj);
+        this.commands = commands;
+        this.serverBased = serverBased;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    public boolean isServerBased() {
+        return serverBased;
     }
 }
