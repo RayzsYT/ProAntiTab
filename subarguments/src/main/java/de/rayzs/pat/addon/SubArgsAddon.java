ca2f139542772a8ca595eaf52112807a2baacdde
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-09-29 14:45:59 +0200

    Add Method

--- subarguments/src/main/java/de/rayzs/pat/addon/SubArgsAddon.java
+++ subarguments/src/main/java/de/rayzs/pat/addon/SubArgsAddon.java
@@ -2,9 +2,14 @@ package de.rayzs.pat.addon;
 
 import de.rayzs.pat.addon.utils.Argument;
 import de.rayzs.pat.api.event.PATEventHandler;
+import de.rayzs.pat.utils.StringUtils;
 import de.rayzs.pat.utils.configuration.*;
 import de.rayzs.pat.api.storage.Storage;
 import de.rayzs.pat.addon.events.*;
+import de.rayzs.pat.utils.group.Group;
+import de.rayzs.pat.utils.group.GroupManager;
+import de.rayzs.pat.utils.permission.PermissionUtil;
+
 import java.util.*;
 
 public class SubArgsAddon {
@@ -39,5 +44,25 @@ public class SubArgsAddon {
         ArrayList<String> MESSAGE = new ArrayList<>();
         MESSAGE.add("&8[&4ProAntiTab&8] &cThis argument is not allowed!");
         BLOCKED_MESSAGE = (List<String>) CONFIGURATION.getOrSet("blocked-message", MESSAGE);
+    }
+
+    public static List<String> getGroupCommands(UUID uuid) {
+        List<String> commands = new ArrayList<>();
+        List<Group> groups = new ArrayList<>();
+
+        Group currentGroup;
+        for (String permission : PermissionUtil.getPermissions(uuid)) {
+            if(!permission.startsWith("proantitab.group.")) continue;
+            currentGroup = GroupManager.getGroupByName(StringUtils.replaceFirst(permission, "proantitab.group.", ""));
+            if(currentGroup != null && !groups.contains(currentGroup)) groups.add(currentGroup);
+        }
+
+        if(!groups.isEmpty()) {
+            int highestPriority = groups.stream().mapToInt(Group::getPriority).filter(group -> group <= 9999).min().orElse(9999);
+            groups.removeIf(group -> group.getPriority() > highestPriority);
+            for (Group group : groups) commands.addAll(group.getCommands().stream().filter(command -> command.contains(" ")).toList());
+        }
+
+        return commands;
     }
 }
