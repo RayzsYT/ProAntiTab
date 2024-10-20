c419444481cf79ffb9967e7c46da0b6b5be70ae7
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-10-20 03:36:42 +0200

    Fixed Pattern-issue with *

--- src/main/java/de/rayzs/pat/utils/StringUtils.java
+++ src/main/java/de/rayzs/pat/utils/StringUtils.java
@@ -7,7 +7,7 @@ public class StringUtils {
 
     public static String replaceFirst(String input, String trigger, String replacement) {
         if (input.contains(trigger)) {
-            String[] split = input.split(trigger);
+            String[] split = input.split(trigger.equals("*") ? "\\*" : trigger);
             if (split.length >= 1) {
                 int pointAfter = split[0].length() + trigger.length();
                 input = split[0] + replacement + input.substring(pointAfter);
