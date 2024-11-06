d5b53aecfa5749ea7e62126c9daad4ad7f398fee
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2024-11-06 23:53:23 +0100

    Removed %rmc_online_player% from executable commands filter

--- src/main/java/de/rayzs/pat/plugin/modules/subargs/events/ExecuteCommand.java
+++ src/main/java/de/rayzs/pat/plugin/modules/subargs/events/ExecuteCommand.java
@@ -45,6 +45,7 @@ public class ExecuteCommand extends ExecuteCommandEvent {
         String[] split, originCommandSplit, copiedOriginCommandSplit;
         String tmpCommand;
         for (String s : SubArgsModule.PLAYER_COMMANDS.getOrDefault(sender.getUniqueId(), Argument.getGeneralArgument()).getInputs()) {
+            if(s.contains("%rmc_online_players%")) continue;
             tmpCommand = command;
 
             if(s.split(" ")[0].equalsIgnoreCase(tmpCommand.split(" ")[0]))
