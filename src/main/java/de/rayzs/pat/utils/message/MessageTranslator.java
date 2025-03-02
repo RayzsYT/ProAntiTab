d5e1ac1968e17d8fc0a0cb04deea1ae15b25b22a
RayzsYT <51991564+RayzsYT@users.noreply.github.com>
2025-03-02 01:15:14 +0100

    Removed PlaceholderAPI request if no placeholder exist

--- src/main/java/de/rayzs/pat/utils/message/MessageTranslator.java
+++ src/main/java/de/rayzs/pat/utils/message/MessageTranslator.java
@@ -1,6 +1,5 @@
 package de.rayzs.pat.utils.message;
 
-import de.rayzs.pat.api.communication.Communicator;
 import de.rayzs.pat.utils.configuration.helper.MultipleMessagesHelper;
 import de.rayzs.pat.utils.message.replacer.PlaceholderReplacer;
 import de.rayzs.pat.utils.message.translators.*;
--- src/main/java/de/rayzs/pat/utils/message/replacer/PlaceholderReplacer.java
+++ src/main/java/de/rayzs/pat/utils/message/replacer/PlaceholderReplacer.java
@@ -11,6 +11,7 @@ public class PlaceholderReplacer {
 
     public static String replace(Object playerObj, String text) {
         if(!Storage.USE_PLACEHOLDERAPI) return text;
+        if(!text.contains("%")) return text;
         if(BUKKIT_PLACEHOLDER_REPLACER == null) BUKKIT_PLACEHOLDER_REPLACER = new BukkitPlaceholderReplacer();
 
         return BUKKIT_PLACEHOLDER_REPLACER.process(playerObj, text);
@@ -18,6 +19,7 @@ public class PlaceholderReplacer {
 
     public static boolean process(Object playerObj, String text, Consumer<String> consumer) {
         if(!Storage.USE_PAPIPROXYBRIDGE) return false;
+        if(!text.contains("%")) return false;
         if(PROXY_PLACEHOLDER_REPLACER == null) PROXY_PLACEHOLDER_REPLACER = new ProxyPlaceholderReplacer();
 
         return PROXY_PLACEHOLDER_REPLACER.process(playerObj, text, consumer);
