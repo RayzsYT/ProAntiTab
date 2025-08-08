74e26a05f3aaeb4c6e6b53b1bc042c83090547b5
rayzsyt <alihan290404@gmail.com>
2025-08-08 04:32:58 +0200

    Update version name

--- src/main/java/de/rayzs/pat/plugin/VelocityLoader.java
+++ src/main/java/de/rayzs/pat/plugin/VelocityLoader.java
@@ -38,7 +38,7 @@ import java.util.*;
 
 @Plugin(name = "ProAntiTab",
 id = "proantitab",
-version = "2.0.8",
+version = "2.0.9",
 authors = "Rayzs_YT",
 description = "Hide more than just your plugins. Hide almost everything!",
 url = "https://www.rayzs.de/products/proantitab/page",
