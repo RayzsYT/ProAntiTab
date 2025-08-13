637b7a263a42806821af49269a99959979324899
rayzsyt <alihan290404@gmail.com>
2025-08-13 20:51:52 +0200

    Improved Update Command

--- src/main/java/de/rayzs/pat/plugin/process/impl/local/info/UpdateCommand.java
+++ src/main/java/de/rayzs/pat/plugin/process/impl/local/info/UpdateCommand.java
@@ -1,9 +1,13 @@
 package de.rayzs.pat.plugin.process.impl.local.info;
 
+import de.rayzs.pat.plugin.listeners.bukkit.BukkitAntiTabListener;
+import de.rayzs.pat.utils.Reflection;
 import de.rayzs.pat.utils.permission.PermissionUtil;
 import de.rayzs.pat.api.command.ProCommand;
 import de.rayzs.pat.api.storage.Storage;
 import de.rayzs.pat.utils.sender.CommandSender;
+import de.rayzs.pat.utils.sender.CommandSenderHandler;
+
 import java.util.*;
 
 public class UpdateCommand extends ProCommand {
@@ -19,17 +23,35 @@ public class UpdateCommand extends ProCommand {
     public boolean execute(CommandSender sender, String[] args) {
         if (args.length == 0) {
             PermissionUtil.reloadPermissions();
+
+            Storage.getLoader().getOnlinePlayerNames().forEach(name -> {
+                Object playerObj = Storage.getLoader().getPlayerObjByName(name);
+
+                if (playerObj != null) {
+                    CommandSender s = CommandSenderHandler.from(playerObj);
+
+                    assert s != null;
+                    PermissionUtil.reloadPermissions(s);
+                }
+            });
+
+            if (!Reflection.isProxyServer()) {
+                BukkitAntiTabListener.handleTabCompletion();
+            }
+
             sender.sendMessage(Storage.ConfigSections.Messages.UPDATE_PERMISSIONS.UPDATE_ALL);
             return true;
         }
 
         String name = args[0];
         UUID uuid = Storage.getLoader().getUUIDByName(name);
+        Object playerObj = Storage.getLoader().getPlayerObjByUUID(uuid);
+        CommandSender targetSender = CommandSenderHandler.from(playerObj);
 
-        boolean exist = uuid != null;
+        boolean exist = targetSender != null;
 
         if (exist) {
-            name = Storage.getLoader().getNameByUUID(uuid);
+            name = targetSender.getName();
         }
 
         String message = exist
@@ -39,7 +61,13 @@ public class UpdateCommand extends ProCommand {
         message = message.replace("%target%", name);
 
         if (exist) {
-            PermissionUtil.reloadPermissions(uuid);
+
+            PermissionUtil.reloadPermissions(targetSender);
+
+            if (!Reflection.isProxyServer()) {
+                BukkitAntiTabListener.handleTabCompletion(uuid);
+            }
+
         }
 
         sender.sendMessage(message);
@@ -48,6 +76,6 @@ public class UpdateCommand extends ProCommand {
 
     @Override
     public List<String> tabComplete(CommandSender sender, String[] args) {
-        return args.length < 2 ? Storage.getLoader().getPlayerNames() : null;
+        return args.length < 2 ? Storage.getLoader().getOnlinePlayerNames() : null;
     }
 }
