560c9c3c8b6239a94a0a45fee2ea514225ad7dac
rayzsyt <alihan290404@gmail.com>
2025-10-30 23:53:16 +0100

    Removed UpdateGroupsPerServer feature for now

--- src/main/java/de/rayzs/pat/api/storage/config/settings/UpdateGroupsPerServerSection.java
+++ src/main/java/de/rayzs/pat/api/storage/config/settings/UpdateGroupsPerServerSection.java
@@ -6,7 +6,8 @@ import de.rayzs.pat.utils.configuration.helper.ConfigSectionHelper;
 
 public class UpdateGroupsPerServerSection extends ConfigStorage {
 
-    public boolean ENABLED;
+    // Disabled for now
+    public boolean ENABLED = false;
 
     public UpdateGroupsPerServerSection() {
         super("update-groups-per-server");
@@ -16,9 +17,11 @@ public class UpdateGroupsPerServerSection extends ConfigStorage {
     public void load() {
         super.load();
 
+        /*
         if (!Reflection.isProxyServer())
             return;
 
         ENABLED = new ConfigSectionHelper<Boolean>(this, null, false).getOrSet();
+        */
     }
 }
