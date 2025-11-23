567ca5192cc6376a20689bddefaafa8ff5110c21
rayzsyt <alihan290404@gmail.com>
2025-11-23 04:42:25 +0100

    Implemented Code

--- src/main/java/de/rayzs/pat/plugin/converter/converters/PluginHiderPlus.java
+++ src/main/java/de/rayzs/pat/plugin/converter/converters/PluginHiderPlus.java
@@ -1,19 +1,32 @@
 package de.rayzs.pat.plugin.converter.converters;
 
+import de.rayzs.pat.api.storage.Storage;
 import de.rayzs.pat.plugin.converter.Converter;
-import de.rayzs.pat.plugin.converter.StorageConverter;
-import de.rayzs.pat.utils.configuration.ConfigurationBuilder;
+import de.rayzs.pat.utils.sender.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PluginHiderPlus extends Converter {
 
     public PluginHiderPlus() {
-        super("PluginHiderPlus", "PluginHiderPlus", "config");
+        super("PluginHider+", "PluginHiderPlus", "config");
     }
 
     @Override
-    public void apply() {
+    public void apply(CommandSender sender) {
 
+        boolean blacklist = (boolean) config.get("reverse-suggestionsWhitelist");
+        Storage.ConfigSections.Settings.TURN_BLACKLIST_TO_WHITELIST.getConfig().setAndSave("turn-blacklist-to-whitelist", !blacklist);
 
+        List<String> commands = (ArrayList<String>) config.get("suggestionsWhitelist");
 
+        if (blacklist) {
+            List<String> blockedCommands = (ArrayList<String>) config.get("blockedCommands.everyone.commands");
+            commands.addAll(blockedCommands);
+        }
+
+        applyStorage(Storage.Blacklist.getBlacklist(), commands);
+        sender.sendMessage("&e&lNotice: &7Since PluginHider+ both blocks certain commands and whitelist a few, the conversion into PAT's storage.yml format won't be accurate. Only whitelisted commands have been added, and blocked commands for everyone if the plugin was used in blacklist mode.");
     }
 }
