187399734886c562bb5906119623c86daa5b41f4
rayzsyt <alihan290404@gmail.com>
2025-12-16 21:39:24 +0100

    Experimental exception catch

--- src/main/java/de/rayzs/pat/api/communication/client/impl/BungeeClientInfo.java
+++ src/main/java/de/rayzs/pat/api/communication/client/impl/BungeeClientInfo.java
@@ -2,6 +2,8 @@ package de.rayzs.pat.api.communication.client.impl;
 
 import de.rayzs.pat.api.communication.client.ClientInfo;
 import de.rayzs.pat.api.communication.Client;
+import de.rayzs.pat.plugin.BungeeLoader;
+import de.rayzs.pat.plugin.logger.Logger;
 import net.md_5.bungee.api.ProxyServer;
 
 public class BungeeClientInfo extends ClientInfo {
@@ -16,11 +18,18 @@ public class BungeeClientInfo extends ClientInfo {
 
     @Override
     public void sendBytes(byte[] bytes) {
-        ProxyServer.getInstance().getServers().entrySet().stream().filter(entry ->
-                entry.getKey().equalsIgnoreCase(getName())
-        ).forEach(entry ->
-                entry.getValue().sendData(Client.CHANNEL_NAME, bytes)
-        );
+        try {
+            ProxyServer server = BungeeLoader.getPlugin().getProxy();
+
+            server.getServers().entrySet().stream().filter(entry ->
+                    entry.getKey().equalsIgnoreCase(getName())
+            ).forEach(entry ->
+                    entry.getValue().sendData(Client.CHANNEL_NAME, bytes)
+            );
+
+        } catch (NoClassDefFoundError exception) {
+            Logger.warning("Failed to send data to backend servers! :c");
+        }
     }
 }
 
