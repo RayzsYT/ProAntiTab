d556290a19bf8f19c71f03f83490e7b9fc578ba2
rayzsyt <alihan290404@gmail.com>
2025-12-29 20:35:59 +0100

    Call handleCommandNode from SubArgsModule

--- src/main/java/de/rayzs/pat/api/netty/bukkit/handlers/ModernCommandsNodeHandler.java
+++ src/main/java/de/rayzs/pat/api/netty/bukkit/handlers/ModernCommandsNodeHandler.java
@@ -1,15 +1,10 @@
 package de.rayzs.pat.api.netty.bukkit.handlers;
 
 import de.rayzs.pat.api.netty.bukkit.BukkitPacketHandler;
+import de.rayzs.pat.plugin.modules.SubArgsModule;
 import de.rayzs.pat.utils.node.BukkitCommandNodeHelper;
 import de.rayzs.pat.utils.sender.CommandSender;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.Nullable;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.util.*;
-
 public class ModernCommandsNodeHandler implements BukkitPacketHandler {
 
     public boolean handleIncomingPacket(Player player, CommandSender sender, Object packetObj) throws Exception {
@@ -21,9 +16,7 @@ public class ModernCommandsNodeHandler implements BukkitPacketHandler {
     public boolean handleOutgoingPacket(Player player, CommandSender sender, Object packetObj) throws Exception {
         final BukkitCommandNodeHelper helper = new BukkitCommandNodeHelper(packetObj);
 
-        helper.removeSubArguments("f admin");
-        helper.removeSubArguments("f ahome");
-
+        SubArgsModule.handleCommandNode(player.getUniqueId(), helper);
         return true;
     }
 }
