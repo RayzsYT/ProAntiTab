eb4b2b1e17a698e3ddf8e94193e97c954bf36611
rayzsyt <alihan290404@gmail.com>
2026-01-15 19:53:32 +0100

    Removed unnecessary computation

--- src/main/java/de/rayzs/pat/plugin/subarguments/events/UpdateList.java
+++ src/main/java/de/rayzs/pat/plugin/subarguments/events/UpdateList.java
@@ -46,35 +46,13 @@ public class UpdateList {
             argument.clearArguments();
 
             if (Storage.ConfigSections.Settings.TURN_BLACKLIST_TO_WHITELIST.ENABLED) {
-                for (String command : event.getCommands())
+                for (String command : event.getCommands()) {
                     argument.buildArgumentStacks(command);
-
-                final List<String> chatInputs = new ArrayList<>(argument.CHAT_ARGUMENTS.getGeneralArgument().getInputs());
-                final List<String> tabInput = new ArrayList<>(argument.TAB_ARGUMENTS.getGeneralArgument().getInputs());
-
-
-                for (String command : chatInputs) {
-                    Storage.Blacklist.BlockType type = Storage.Blacklist.BlockTypeFetcher.getType(command);
-
-                    if (type != Storage.Blacklist.BlockType.NEGATE) {
-                        command = Storage.Blacklist.BlockTypeFetcher.modify(command, type);
-                    }
-
-                    argument.CHAT_ARGUMENTS.buildArguments(command);
                 }
 
-                for (String command : tabInput) {
-                    Storage.Blacklist.BlockType type = Storage.Blacklist.BlockTypeFetcher.getType(command);
-
-
-                    if (type != Storage.Blacklist.BlockType.NEGATE)
-                        command = Storage.Blacklist.BlockTypeFetcher.modify(command, type);
-
-                    argument.TAB_ARGUMENTS.buildArguments(command);
-                }
-
-                for (String command : SubArguments.getGroupCommands(uuid, serverName))
+                for (String command : SubArguments.getGroupCommands(uuid, serverName)) {
                     argument.buildArgumentStacks(command);
+                }
 
                 for (String command : SubArguments.getServerCommands(uuid)) {
                     argument.buildArgumentStacks(command);
