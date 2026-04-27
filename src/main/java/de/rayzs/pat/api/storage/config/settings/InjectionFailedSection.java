0599d504aff4e866bb1113215bd7e1fd87230753
rayzsyt <alihan290404@gmail.com>
2026-04-27 23:18:29 +0200

    Fixed that messages were not set automatically in auto-config-updater

--- src/main/java/de/rayzs/pat/api/storage/config/settings/InjectionFailedSection.java
+++ src/main/java/de/rayzs/pat/api/storage/config/settings/InjectionFailedSection.java
@@ -1,13 +1,12 @@
 package de.rayzs.pat.api.storage.config.settings;
 
 import de.rayzs.pat.api.storage.storages.ConfigStorage;
-import de.rayzs.pat.utils.StringUtils;
 import de.rayzs.pat.utils.configuration.helper.ConfigSectionHelper;
 
 public class InjectionFailedSection extends ConfigStorage {
 
     public boolean ENABLED, SUPPRESS_EXCEPTIONS;
-    public ConfigSectionHelper<String> KICK_MESSAGE, CONSOLE_MESSAGE;
+    public String KICK_MESSAGE, CONSOLE_MESSAGE;
 
     public InjectionFailedSection() {
         super("injection-failed");
@@ -18,7 +17,7 @@ public class InjectionFailedSection extends ConfigStorage {
         super.load();
         ENABLED = new ConfigSectionHelper<Boolean>(this, "kick-on-failure", true).getOrSet();
         SUPPRESS_EXCEPTIONS = new ConfigSectionHelper<Boolean>(this, "suppress-exceptions", false).getOrSet();
-        KICK_MESSAGE = new ConfigSectionHelper<>(this, "kick-message", "&cInjection process failed! Please reconnect.");
-        CONSOLE_MESSAGE = new ConfigSectionHelper<>(this, "console-message", "&cInjection process failed! (%player%)");
+        KICK_MESSAGE = new ConfigSectionHelper<String>(this, "kick-message", "&cInjection process failed! Please reconnect.").getOrSet();
+        CONSOLE_MESSAGE = new ConfigSectionHelper<String>(this, "console-message", "&cInjection process failed! (%player%)").getOrSet();
     }
 }
